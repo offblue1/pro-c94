841e6ba800bd2beb94f21f9da646855677db80c3
offblue1 <81396226+offblue1@users.noreply.github.com>
2021-12-30 10:38:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCtu922JXe8m2hC_XjLrY-DAcY-YZwyJyA",
+  authDomain: "story-c0025.firebaseapp.com",
+  databaseURL: "https://story-c0025-default-rtdb.firebaseio.com",
+  projectId: "story-c0025",
+  storageBucket: "story-c0025.appspot.com",
+  messagingSenderId: "535763936672",
+  appId: "1:535763936672:web:08b26dfac038bb8772f0c5"
+};
